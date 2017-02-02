3a9a47db8d6bc9d280c0b1f3fae1e66fd0cb4e11
Rose Marie <Rose_PC@lenovo.com>
2017-02-02 12:16:51 -0600

    added song component

--- iTunes.js
+++ iTunes.js
@@ -2,13 +2,11 @@ function Search() {
 	// $.ajax({
 	// 	type: 'GET',
  //        dataType: 'json',
- //        url: 'https://itunes.apple.com/search?term=' + searchConvert($('#searchInput').val()) + '&media=music'+'&limit=20'+"&callback=?",       
+ //        url: 'https://itunes.apple.com/search?term='       
  //        success: function(items)
 
 		var encodedSearch = '';
-		var searchTerm = {
-			term: jQuery('#search-keyword').val(),
-		};
+		var searchTerm = { term: jQuery('#search-keyword').val(), };
 		$.each(searchTerm, function(key, value){
 			encodedSearch += encodeURIComponent(key) + '=' + encodeURIComponent(value) + '&';
 		})
@@ -24,13 +22,14 @@ function SearchResults(arg)
 		var results = arg.results;
 		var htmlDisplayResults = '';
 
-		$.each(results, function(key, value){
+		$.each(results, function(key, value)
+		{
 			htmlDisplayResults += '<li class="htmlDisplayResults col-md-3">';
 			htmlDisplayResults += '<a href="' + value.trackViewUrl + '"target="_blank"><img src="' + value.artworkUrl100 + '"></a>'+ '<br>';
-			htmlDisplayResults += '<audio controls><source src="' + value.previewUrl + '"type="audio/mp4">Preview</audio>';
+			htmlDisplayResults += '<audio controls = "" width = "50%"><source src="' + value.previewUrl + '"type="audio/mp4">Preview</audio>';
 			htmlDisplayResults += '<span style="color:white">' +'<p>ARTIST: ' + '<a href="' + value.artistViewUrl + '" target="_blank">' + value.artistName + '</p></a>';
-			htmlDisplayResults += '<span style="color:white">' + '<p>SONG: ' + value.songName + '</span></p>';
-			htmlDisplayResults += '<span style="color:white">' + '<p>GENRE: ' + value.primaryGenreName + '</span></p><br>';
+			htmlDisplayResults += '<span style="color:white">' + '<p>SONG: ' + value.trackName + '</span></p>';
+			// htmlDisplayResults += '<span style="color:white">' + '<p>GENRE: ' + value.primaryGenreName + '</span></p><br>';
 			htmlDisplayResults += '</li>';
 		})
 		jQuery('#itunes-results').html(htmlDisplayResults);
